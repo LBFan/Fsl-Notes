04ff3fb9ffa4bdb5e75cb0173329c4702fa5bdb8
LBFan <fanshulin_2016@163.com>
2020-05-28 12:59:58 +0800

    增加注释

--- src/focusonoffer/Num03Plus.java
+++ src/focusonoffer/Num03Plus.java
@@ -22,19 +22,25 @@ public class Num03Plus {
             return -1;
         }
         int start = 0, end = length - 1;
+        // 当end >= start时,循环
         while (start <= end) {
             int mid = (end - start) / 2 + start;
+            // 统计整个数组中>= start + 1 && <= mid + 1的数字的个数
             int count = countRange(nums, length, start, mid);
             if (end == start) {
+                // end == start == mid,count > 1,说明大小为start的值不止一个,即为所求值,返回即可
                 if (count > 1) {
                     return start;
                 } else {
+                    // 大小为start的值最多存在一个,不为所求值,退出循环,表示未找到重复值,返回-1
                     break;
                 }
             }
+            // 区间存在的个数 > 区间长度,说明重复值在该区间,end = mid
             if (count > (mid - start + 1)) {
                 end = mid;
             } else {
+                // 区间存在的个数 < 区间长度,说明重复值在该区间,start = mid + 1
                 start = mid + 1;
             }
         }
