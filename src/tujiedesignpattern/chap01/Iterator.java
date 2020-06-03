2c63250183973ea0017c6159aa500100476b704a
xmly <shulin.fan@ximalaya.com>
2020-06-03 09:31:44 +0800

    图解设计模式之iterator模式

---
+++ src/tujiedesignpattern/chap01/Iterator.java
@@ -0,0 +1,22 @@
+package tujiedesignpattern.chap01;
+
+/**
+ * @Description :
+ * @Author : fanshulin
+ * @Date : 2020/6/2
+ */
+public interface Iterator {
+    /**
+     * 是否还有没有遍历的元素
+     *
+     * @return 还有未迭代的元素：true | 没有未迭代的元素：false
+     */
+    boolean hasNext();
+
+    /**
+     * next
+     *
+     * @return 下一个迭代元素
+     */
+    Object next();
+}
---
+++ src/tujiedesignpattern/chap01/Main.java
@@ -0,0 +1,23 @@
+package tujiedesignpattern.chap01;
+
+/**
+ * @Description :
+ * @Author : fanshulin
+ * @Date : 2020/6/2
+ */
+public class Main {
+    public static void main(String[] args) {
+        BookSelf bookSelf = new BookSelf(4);
+        bookSelf.appendBook(new Book("Around the world"));
+        bookSelf.appendBook(new Book("Bible"));
+        bookSelf.appendBook(new Book("China"));
+        bookSelf.appendBook(new Book("July"));
+
+        Iterator  iterator = bookSelf.iterator();
+
+        while (iterator.hasNext()) {
+            Book book = (Book) iterator.next();
+            System.out.println(book.getName());
+        }
+    }
+}
