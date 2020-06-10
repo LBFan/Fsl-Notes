5f54b85c0ea560cbf673ea043f8b963cd3250aff
xmly <shulin.fan@ximalaya.com>
2020-06-10 21:43:37 +0800

    【新增】oneEditWay, 字符串压缩后返回长度较短的字符串 aabbbcc -> a2b3c2;abc->abc

--- src/crackingthecodinginterview/chap01/Chap01Test.java
+++ src/crackingthecodinginterview/chap01/Chap01Test.java
@@ -34,4 +34,28 @@ public class Chap01Test {
         String result= Num03.replaceSpaces(s);
         Assert.assertEquals("er%20tyu%20io", result);
     }
+
+    @Test
+    public void testNum04() {
+        String s = "tactact";
+        boolean palindromePermutation1 = Num04.palindromePermutation1(s);
+        Assert.assertTrue(palindromePermutation1);
+    }
+
+    @Test
+    public void testNum05() {
+        String first = "ple", second = "pale";
+        boolean b = Num05.oneEditWay(first, second);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void testNum06() {
+        String s = "aaabbbbbcc";
+        String t = "abc";
+        String compress = Num06.compress(s);
+        Assert.assertEquals("a3b5c2", compress);
+        String compress1 = Num06.compress(t);
+        Assert.assertEquals("abc", compress1);
+    }
 }
