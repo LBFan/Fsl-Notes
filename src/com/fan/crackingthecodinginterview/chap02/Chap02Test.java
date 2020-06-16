fb41a7b078cae9b2d79a0bd5758be94ea7a5f91b
LBFan <fanshulin_2016@163.com>
2020-06-16 23:05:30 +0800

    添加chap02测试代码

--- src/com/fan/crackingthecodinginterview/chap02/Chap02Test.java
+++ src/com/fan/crackingthecodinginterview/chap02/Chap02Test.java
@@ -1,5 +1,7 @@
 package com.fan.crackingthecodinginterview.chap02;
 
+import com.fan.util.ListNode;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,7 +13,21 @@ import org.junit.Test;
 public class Chap02Test {
     @Test
     public void testNum01() {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(1);
+        ListNode l3 = new ListNode(2);
+        ListNode l4 = new ListNode(3);
+        ListNode l5 = new ListNode(4);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
 
+        ListNode listNode = Num01.removeDups(l1);
+        l1.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        Assert.assertNotSame(l1, listNode);
     }
 }
 
