be38ff0594435446f944726345aa8682711f36cb
LBFan <fanshulin_2016@163.com>
2020-08-17 12:40:05 +0800

    push;

---
+++ src/com/fan/aqs/ThreadPoolTest.java
@@ -0,0 +1,18 @@
+package com.fan.aqs;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author : PF_23
+ * @Description :
+ * @date : 2020/07/15.
+ */
+
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+    }
+}
+
