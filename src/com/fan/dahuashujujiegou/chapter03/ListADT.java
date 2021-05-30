ce0d6ea2436be5d61233951ab9faed81eb3feede
LBFan <fanshulin_2016@163.com>
2021-05-30 21:44:57 +0800

    【大话数据结构】第三章

---
+++ src/com/fan/dahuashujujiegou/chapter03/ListADT.java
@@ -0,0 +1,22 @@
+package com.fan.dahuashujujiegou.chapter03;
+
+/**
+ * @author : PF_23
+ * @Description : 线性表的抽象数据类型。有如下定义：每个元素类型为简单类型整数型。除第一个元素外，每个元素有且只有一个前驱元素，除最后一个元素外，每个元素有且只有一个后继元素
+ * 有如下操作：
+ *  initList(int[] list) : 初始化操作，建立一个空的线性表
+ *  listEmpty(list) : 为空返回true，否则返回false
+ *  cleanList(list) : 清空线性表
+ *  getElem(list, i) : 将线性表中的第i个位置的元素返回
+ *  locateElem(list, k) : 查找线性表中值等于给定元素k，成功则返回位置，否则返回-1表示失败
+ *  listInsert(list, i, k) : 在线性表list中的第i个位置插入元素k
+ *  listDelete(list, i) ： 删除线性表中位置为i的元素并且返回当前删除元素的值
+ *  listLength(list) : 返回当前线性表的个数
+ * @date : 2021/5/29 09:47.
+ */
+
+public class ListADT {
+
+
+}
+
